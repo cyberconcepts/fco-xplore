b00098a877162a7e9fd565934b3c9d18393a834b
Helmut Merz <helmutm@cy55.de>
2018-06-27 15:06:17 +0200

    ForthProxy now basically working

--- src/Messaging/ForthProxy.hs
+++ src/Messaging/ForthProxy.hs
@@ -5,22 +5,29 @@ module Messaging.ForthProxy where
 import BasicPrelude
 import Control.Concurrent (forkIO)
 import Control.Monad (forever)
-import GHC.IO.Handle (
+import System.IO (
     BufferMode (NoBuffering), Handle,
-    hSetBuffering, hFlush, hGetContents, hGetLine, hPutStr)
+    hSetBuffering, hFlush, hGetContents, hGetLine, hPutStr, openFile)
 import System.Process (
     StdStream (CreatePipe), 
     proc, shell, std_in, std_out, withCreateProcess)
 import Data.Text (unpack)
 
 
+forthHome = "~/development/forth/"
+forthModule = "xplore/interact.fs"
+forthCommand = "gforth " ++ forthHome ++ forthModule ++ " -e repl"
+
+
 run :: IO ()
-run = withCreateProcess (shell "/usr/local/bin/gforth") 
-  { std_in = CreatePipe, std_out = CreatePipe } 
-  $ \(Just hIn) (Just hOut) _ hProc -> do
-    hSetBuffering hIn NoBuffering
-    forkIO $ process4thOutput hOut
-    provide4thInput hIn
+run = 
+  withCreateProcess (shell forthCommand)
+    { std_in = CreatePipe, std_out = CreatePipe } 
+    $ \(Just hIn) (Just hOut) _ hProc -> do
+      hSetBuffering hIn NoBuffering
+      hSetBuffering hOut NoBuffering
+      forkIO $ process4thOutput hOut
+      provide4thInput hIn
 
 
 provide4thInput :: Handle -> IO ()
@@ -28,9 +35,8 @@ provide4thInput handle = do
   s <- getLine
   case s of 
     "bye" -> hPutStr handle "bye\n"
-    _ -> do 
-      hPutStr handle $ unpack (s ++ "\n")
-      provide4thInput handle
+    _ -> (hPutStr handle $ unpack (s ++ "\n")) >>
+         provide4thInput handle
 
 
 process4thOutput :: Handle -> IO ()
