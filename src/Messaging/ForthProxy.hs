ebf608bf5a178ea5c2aec628dd22c0b2f27ced24
Helmut Merz <helmutm@cy55.de>
2018-07-10 19:07:02 +0200

    wrap ForthProxy in  extensible effect (with freer-simple)

--- src/Messaging/ForthProxy.hs
+++ src/Messaging/ForthProxy.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE NoImplicitPrelude, OverloadedStrings #-}
+{-# LANGUAGE DataKinds, FlexibleContexts, GADTs #-}
 
 module Messaging.ForthProxy where
 
 import BasicPrelude
-import Data.Text (unpack)
+import Data.Text (pack, unpack)
 
 import Control.Monad (forever)
+
+import Control.Monad.Freer (Eff, Member, interpretM, runM, send)
 
 import Control.Distributed.Process (Process, liftIO)
 import Control.Distributed.Process.Backend.SimpleLocalnet (
@@ -15,7 +18,7 @@ import Control.Distributed.Process.Node (
 
 import System.IO (
     BufferMode (NoBuffering), Handle,
-    hSetBuffering, hFlush, hGetContents, hGetLine, hPutStr, openFile)
+    hSetBuffering, hGetLine, hPutStr)
 import System.Process (
     StdStream (CreatePipe), 
     proc, shell, std_in, std_out, withCreateProcess)
@@ -29,17 +32,31 @@ host = "127.0.0.1"
 port = "8899"
 
 
-run :: IO ()
-run = do
+data ForthProxy r where 
+  StartProxy :: String -> ForthProxy ()
+
+startProxy :: Member ForthProxy effs => String -> Eff effs ()
+startProxy cmd = send (StartProxy cmd)
+
+runForthProxy :: Eff '[ForthProxy, IO] a -> IO a
+runForthProxy = runM . interpretM (\c ->
+    case c of 
+        StartProxy cmd -> doStartProxy cmd)
+
+doStartProxy :: String -> IO ()
+doStartProxy cmd = do
   backend <- initializeBackend host port initRemoteTable
   node <- newLocalNode backend
-  withCreateProcess (shell forthCommand)
+  withCreateProcess (shell cmd)
     { std_in = CreatePipe, std_out = CreatePipe } 
     $ \(Just hIn) (Just hOut) _ hProc -> do
       hSetBuffering hIn NoBuffering
       hSetBuffering hOut NoBuffering
       pid <- forkProcess node $ process4thOutput hOut 
       runProcess node $ provide4thInput hIn
+
+run :: IO ()
+run = runForthProxy $ startProxy forthCommand
 
 
 provide4thInput :: Handle -> Process ()
