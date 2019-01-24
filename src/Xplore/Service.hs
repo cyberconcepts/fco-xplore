d7e5f81a72b33f6044a0ad77f2e6bcae865e4926
Helmut Merz <helmutm@cy55.de>
2019-01-24 16:01:41 +0100

    minimal channel and service implementation

--- src/Xplore/Service.hs
+++ src/Xplore/Service.hs
@@ -5,5 +5,44 @@ module Xplore.Service where
 import BasicPrelude
 import qualified Data.Text as T
 
-import Control.Concurrent.STM.TChan
+import Control.Concurrent (ThreadId, forkIO)
+import Control.Concurrent.STM (
+    TChan,
+    atomically, newTChan, readTChan, writeTChan)
+import Control.Monad.Extra (whileM)
 import Control.Monad.STM
+
+
+startService :: IO () -> IO ThreadId
+startService = forkIO
+
+newChan :: IO (TChan a)
+newChan = atomically newTChan
+
+receive :: TChan a -> IO a
+receive = atomically . readTChan
+
+send :: TChan a -> a -> IO ()
+send chan msg = atomically $ writeTChan chan msg
+
+
+conIn :: TChan Text -> IO ()
+conIn chan =
+  whileM $ do
+    line <- getLine
+    case line of
+      "bye" -> send chan "quit" >> return False
+      _ -> send chan line >> return True
+
+conOut :: TChan Text -> IO ()
+conOut chan =
+  whileM $ do
+    line <- receive chan
+    case line of
+      "quit" -> return False
+      _ -> putStrLn line >> return True
+
+demo = do
+  ch <- newChan
+  startService $ conOut ch
+  startService $ conIn ch
--- test/Xplore/ServiceSpec.hs
+++ test/Xplore/ServiceSpec.hs
@@ -3,17 +3,22 @@
 module Xplore.ServiceSpec (main, spec) where
 
 import Test.Hspec
-
 import Control.Exception (evaluate)
+
+import Xplore.Service
+
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
 -- not needed for automatic spec discovery.
 main :: IO ()
 main = hspec spec
 
+
 spec :: Spec
 spec = do
 
-  describe "dummy" $ do
-    it "does nothing" $ do
-      True `shouldBe` True
+  describe "channel" $ do
+    it "accepts and delivers a message" $ do
+      c1 <- newChan
+      send c1 7 `shouldReturn` ()
+      receive c1 `shouldReturn` 7
